0e1b25b0a3d44bd513fea2ac784c716b44e3fec0
Soumik Mitra <soumikdeb2@gmail.com>
2024-01-18 13:03:22 -0500

    Changes made to activity_contacts.xml and aboutUs.java

--- app/src/main/java/com/example/mathprojectraoli/aboutUs.java
+++ app/src/main/java/com/example/mathprojectraoli/aboutUs.java
@@ -1,9 +1,14 @@
 package com.example.mathprojectraoli;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 
 public class aboutUs extends AppCompatActivity {
@@ -13,15 +18,21 @@ public class aboutUs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
 
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#000000"));
+        actionBar.setBackgroundDrawable(colorDrawable);
+
         View aboutPage = new AboutPage(this)
                 .isRTL(false)
                 .setDescription(
                         "This app is intended to be a supplement for the teaching " +
-                                "and learning of Math 174 Discrete Math for Computer " +
-                                "Science at the University of South Carolina Aiken " +
-                                "It started with an assignment of CSCI 340 Mobile Application " +
-                                "Development at the University of South Carolina Aiken " +
-                                "and evolved into current form."
+                        "and learning of Math 174 Discrete Math for Computer " +
+                        "Science at the University of South Carolina Aiken. " +
+                        "It started with an assignment of CSCI 340 Mobile Application " +
+                        "Development at the University of South Carolina Aiken " +
+                        "and evolved into current form."
                 )
                 .addGroup("CONNECT WITH US!")
                 .addEmail("RaoL@usca.edu")
